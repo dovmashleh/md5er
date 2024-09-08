62a92713a08bae8f4f3b3d926ccc18dc78b570ff
Денис Моторыгин <d.motorygin@qsoft.ru>
2024-09-08 20:03:57 +0300

    post md5 endpoint

--- internal/servers/tcp/handlers/md5handler.go
+++ internal/servers/tcp/handlers/md5handler.go
@@ -1,9 +1,37 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"unsafe"
+)
+
+type MD5PostRequest struct {
+	DataString string `json:"text" validate:"required|string"`
+	DataBase64 []byte `json:"base64" validate:"sometimes"`
+}
+type MD5Response struct {
+	Hash   string `json:"hash"`
+	Base64 []byte `json:"base64"`
+}
 
 func (r *Routing) postMD5(ctx *gin.Context) {
+	req := &MD5PostRequest{}
+	if err := ctx.ShouldBindBodyWithJSON(req); err != nil {
+		r.errorResponse(ctx, 400, "json", err)
+		return
+	}
+	if !r.validate(ctx, req) {
+		return
+	}
+	dataSlice := unsafe.Slice(unsafe.StringData(req.DataString), len(req.DataString))
+	hash := r.Md5service.AsByteArray(dataSlice)
 
+	resp := &MD5Response{
+		Hash:   fmt.Sprintf("%x", hash),
+		Base64: hash[:],
+	}
+	ctx.JSON(200, resp)
 }
 func (r *Routing) getMD5(ctx *gin.Context) {
 
--- internal/servers/tcp/handlers/routing.go
+++ internal/servers/tcp/handlers/routing.go
@@ -1,9 +1,14 @@
 package handlers
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/gookit/validate"
 	"md5er/internal/md5"
+	"net/http"
 )
+
+var ErrValidation = errors.New("validation error")
 
 type Routing struct {
 	Md5service *md5.MD5service
@@ -26,4 +31,56 @@ func (r *Routing) makeRouter() *gin.Engine {
 	api.GET("/md5", r.getMD5)
 	api.GET("/health", r.health)
 	return router
+}
+
+type Response struct {
+	Status string        `json:"status"`
+	Data   *ResponseData `json:"data,omitempty"`
+}
+
+type ResponseData struct {
+	Code    string `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+	Content any    `json:"content,omitempty"`
+}
+
+func (r *Routing) successResponse(ctx *gin.Context, status int, data ...any) {
+	res := Response{Status: "ok"}
+
+	if len(data) > 0 {
+		res.Data = &ResponseData{Content: data[0]}
+	}
+
+	ctx.JSON(status, res)
+}
+
+func (r *Routing) errorResponse(ctx *gin.Context, status int, errorCode string, err error, errorsData ...any) {
+
+	res := Response{
+		Status: "error",
+		Data: &ResponseData{
+			Code:    errorCode,
+			Message: err.Error(),
+		},
+	}
+
+	if len(errorsData) > 0 {
+		res.Data.Content = errorsData[0]
+	}
+
+	ctx.JSON(status, res)
+}
+
+func (r *Routing) validationErrorResponse(ctx *gin.Context, errs validate.Errors) {
+	r.errorResponse(ctx, http.StatusBadRequest, "validation", ErrValidation, errs)
+}
+
+func (r *Routing) validate(ctx *gin.Context, req any) bool {
+	v := validate.Struct(req)
+	if !v.Validate() {
+		r.validationErrorResponse(ctx, v.Errors)
+		return false
+	}
+
+	return true
 }
